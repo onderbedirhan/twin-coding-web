06e4475c3aef5e1f096fb40185fb6f594f548662
Bedirhan Önder <63157533+onderbedirhan@users.noreply.github.com>
2022-08-11 17:24:07 +0300

    Add experiment filter

--- src/pages/experiment/ExperimentPage.js
+++ src/pages/experiment/ExperimentPage.js
@@ -7,6 +7,45 @@ import ExperimentAppBar from "../../layout/appbars/ExperimentAppBar";
 import Experiments from "../../assets/json/experiments_v2_tr.json";
 
 export default function ExperimentPage() {
+  const cardList = useSelector((state) => state.kitSelection.value);
+  const moduleList = useSelector((state) => state.moduleSelection.value);
+  const sampleList = [];
+  console.log(cardList);
+  console.log(moduleList);
+
+  for (let i = 0; i < cardList.length; i++) {
+    if (cardList[i].checked) {
+      // console.log(cardList[i].ids);
+      for (let j = 0; j < cardList[i].ids.length; j++) {
+        if (sampleList.indexOf(cardList[i].ids[j]) === -1) {
+          sampleList.push(cardList[i].ids[j]);
+        }
+      }
+    }
+  }
+
+  for (let i = 0; i < moduleList.length; i++) {
+    if (moduleList[i].checked) {
+      // console.log(moduleList[i].ids);
+      if (sampleList.indexOf(moduleList[i].ids[0]) === -1) {
+        sampleList.push(moduleList[i].ids[0]);
+      }
+    }
+  }
+
+  function compareTwoArray(value) {
+    return value.every((element) => {
+      if (sampleList.includes(element)) {
+        return true;
+      }
+
+      return false;
+    });
+  }
+
+  console.log("sample");
+  console.log(sampleList);
+
   const selectDifficulty = (difficulty) => {
     if (difficulty == 1) {
       return "Kolay";
@@ -19,15 +58,17 @@ export default function ExperimentPage() {
   return (
     <Grid container>
       <ExperimentAppBar />
-      {Experiments.map((experimentCard) => (
-        <ExperimentCard
-          key={experimentCard.id}
-          title={experimentCard.title}
-          imgSrc={experimentCard.imageName}
-          duration={experimentCard.duration}
-          difficulty={selectDifficulty(experimentCard.difficulty)}
-        />
-      ))}
+      {Experiments.map((experimentCard) =>
+        compareTwoArray(experimentCard.requiredIds) ? (
+          <ExperimentCard
+            key={experimentCard.id}
+            title={experimentCard.title}
+            imgSrc={experimentCard.imageName}
+            duration={experimentCard.duration}
+            difficulty={selectDifficulty(experimentCard.difficulty)}
+          />
+        ) : null
+      )}
     </Grid>
   );
 }
